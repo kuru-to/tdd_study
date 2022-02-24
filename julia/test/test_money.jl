c63ac52e5373b0f94a2494dface0668772972e19
kuru-to <ideruf96@gmail.com>
2022-02-24 10:14:25 +0900

    Pass rate test and add same currency reduce test

--- julia/test/test_money.jl
+++ julia/test/test_money.jl
@@ -12,6 +12,14 @@ module TestMoney
             @test MoneyModule.times(MoneyModule.franc(5), 2) == MoneyModule.franc(5 * 2)
             @test MoneyModule.dollar(5) != MoneyModule.franc(5)
         end
+
+        @testset "bank reduce" begin
+            aBank = MoneyModule.Bank(Dict())
+            MoneyModule.add_rate!(aBank, MoneyModule.Pair("USD", "CHF"), 2.0)
+            test_dollar = MoneyModule.dollar(2)
+            @test MoneyModule.reduce(aBank, test_dollar, "CHF") == MoneyModule.franc(4)
+            @test MoneyModule.reduce(aBank, test_dollar, test_dollar.currency) == test_dollar
+        end
     end
 end
 
