6ef0896b0475d2ad72cd583f354963222c4a7a58
kuru-to <ideruf96@gmail.com>
2022-02-26 11:58:21 +0900

    Add minus method

--- julia/src/JuliaMoney.jl
+++ julia/src/JuliaMoney.jl
@@ -12,9 +12,17 @@ Attributes:
     amount: 金額
     currency: 通貨
 """
-struct Money <: Expression
-    amount::Int
+struct Money{T<:Real} <: Expression
+    amount::T
     currency::String
+end
+
+function dollar(amount::Real)::Money
+    return Money(amount, "USD")
+end
+
+function franc(amount::Real)::Money
+    return Money(amount, "CHF")
 end
 
 """
@@ -33,20 +41,12 @@ end
 """
 `Money`の掛け算
 """
-function times(aMoney::Money, multiplier::Int)::Money
+function times(aMoney::Money, multiplier::Real)::Money
     return Money(aMoney.amount * multiplier, aMoney.currency)
 end
 
-function times(aSum::Sum, multiplier::Int)::Sum
+function times(aSum::Sum, multiplier::Real)::Sum
     return Sum(times(aSum.augend, multiplier), times(aSum.addend, multiplier))
-end
-
-function dollar(amount::Int)::Money
-    return Money(amount, "USD")
-end
-
-function franc(amount::Int)::Money
-    return Money(amount, "CHF")
 end
 
 """
@@ -57,18 +57,32 @@ function add(augend::Expression, addend::Expression)::Expression
 end
 
 """
-銀行が通貨の変換を行う際の通貨のペア
+`Money` の金額の符号を反転させる
+
+引き算に使用
 """
-struct CurrencyPair
-    from_currency::String
-    to_currency::String
+function negative(aMoney::Money)::Expression
+    return Money(-aMoney.amount, aMoney.currency)
 end
 
 """
-ペアの from/to を逆転
+`Sum` の金額の符号を反転させる
+
+各要素の金額の符号を反転させることで対応
 """
-function reverse(aCurrencyPair::CurrencyPair)::CurrencyPair
-    return CurrencyPair(aCurrencyPair.to_currency, aCurrencyPair.from_currency)
+function negative(aSum::Sum)::Expression
+    return Sum(negative(aSum.augend), negative(aSum.addend))
+end
+
+"""
+通貨同士の引き算
+
+Args:
+    minuend: 被減数. 引かれる数
+    subtrahend: 減数. 引く数
+"""
+function minus(minuend::Expression, subtrahend::Expression)::Expression
+    return Sum(minuend, negative(subtrahend))
 end
 
 """
@@ -76,7 +90,7 @@ end
 レート辞書は初期化する
 """
 mutable struct Bank
-    dct_rate::Dict{CurrencyPair, Number}
+    dct_rate::Dict{Tuple{String, String}, Number}
     Bank() = new(Dict())
 end
 
@@ -85,35 +99,40 @@ end
 
 逆の場合も入れておく
 """
-function add_rate!(aBank::Bank, aCurrencyPair::CurrencyPair, rate::Number)
-    aBank.dct_rate[aCurrencyPair] = rate
-    aBank.dct_rate[reverse(aCurrencyPair)] = 1 / rate
+function add_rate!(aBank::Bank, from_currency::String, to_currency::String, rate::Number)
+    aBank.dct_rate[(from_currency, to_currency)] = rate
+    aBank.dct_rate[(to_currency, from_currency)] = 1 / rate
 end
 
 """
 銀行から対象の通貨組み合わせからレートを取得
 
 同じ通貨同士の変換は, レート1として出力
+
+Args:
+    aBank: 変換のレートを保持している銀行インスタンス. 同じ通貨の場合銀行を通す必要がないため,
+        デフォルトとしてレートを持たない銀行インスタンスを格納しておく
 """
-function rate(aBank::Bank, aCurrencyPair::CurrencyPair)::Number
-    if aCurrencyPair.from_currency == aCurrencyPair.to_currency
+function rate(aBank::Bank, from_currency::String, to_currency::String)::Number
+    if from_currency == to_currency
         return 1
     end
-    return aBank.dct_rate[aCurrencyPair]
+    return aBank.dct_rate[(from_currency, to_currency)]
 end
 
 """
 `Money`を縮約して通貨に変換
 """
 function reduce(aBank::Bank, aMonay::Money, to_currency::String)::Money
-    return Money(aMonay.amount * rate(aBank, CurrencyPair(aMonay.currency, to_currency)), to_currency)
+    return Money(aMonay.amount * rate(aBank, aMonay.currency, to_currency), to_currency)
 end
 
 """
 `Sum`を縮約して通貨に変換
 """
 function reduce(aBank::Bank, aSum::Sum, to_currency::String)::Money
-    return Money(reduce(aBank, aSum.augend, to_currency).amount + reduce(aBank, aSum.addend, to_currency).amount, to_currency)
+    sum_amount = reduce(aBank, aSum.augend, to_currency).amount + reduce(aBank, aSum.addend, to_currency).amount
+    return Money(sum_amount, to_currency)
 end
 
 end
--- julia/test/runtests.jl
+++ julia/test/runtests.jl
@@ -1,7 +1,7 @@
 using JuliaMoney
 using Test
 
-@testset "times" begin
+@testset "dollar times" begin
     @test JuliaMoney.times(JuliaMoney.dollar(5), 2) == JuliaMoney.dollar(5 * 2)
     @test JuliaMoney.times(JuliaMoney.dollar(1), 3) == JuliaMoney.dollar(1 * 3)
 end
@@ -11,26 +11,30 @@ end
     @test JuliaMoney.dollar(5) != JuliaMoney.franc(5)
 end
 
-@testset "bank reduce" begin
+@testset "Bank reduce" begin
     aBank = JuliaMoney.Bank()
-    JuliaMoney.add_rate!(aBank, JuliaMoney.CurrencyPair("USD", "CHF"), 2.0)
+    rate = 2.0
+    JuliaMoney.add_rate!(aBank, "USD", "CHF", rate)
     test_dollar = JuliaMoney.dollar(2)
-    @test JuliaMoney.reduce(aBank, test_dollar, "CHF") == JuliaMoney.franc(2* 2)
+    @test JuliaMoney.reduce(aBank, test_dollar, "CHF") == JuliaMoney.franc(2 * rate)
     @test JuliaMoney.reduce(aBank, test_dollar, test_dollar.currency) == test_dollar
 end
 
-@testset "add" begin
+@testset "same currency add" begin
     aBank = JuliaMoney.Bank()
-    JuliaMoney.add_rate!(aBank, JuliaMoney.CurrencyPair("USD", "CHF"), 2.0)
     five_dollar = JuliaMoney.dollar(5)
     @test JuliaMoney.reduce(aBank, JuliaMoney.add(five_dollar, five_dollar), "USD") == JuliaMoney.dollar(5 + 5)
+    three_franc = JuliaMoney.franc(3)
+    two_franc = JuliaMoney.franc(2)
+    @test JuliaMoney.reduce(aBank, JuliaMoney.add(three_franc, two_franc), "CHF") == JuliaMoney.franc(3 + 2)
 end
 
 @testset "different currency add" begin
     aBank = JuliaMoney.Bank()
-    JuliaMoney.add_rate!(aBank, JuliaMoney.CurrencyPair("USD", "CHF"), 2.0)
+    rate = 2.0
+    JuliaMoney.add_rate!(aBank, "USD", "CHF", rate)
     test_money = JuliaMoney.add(JuliaMoney.dollar(5), JuliaMoney.franc(10))
-    @test JuliaMoney.reduce(aBank, test_money, "USD") == JuliaMoney.dollar(10)
+    @test JuliaMoney.reduce(aBank, test_money, "USD") == JuliaMoney.dollar(5 + 10 / rate)
 end
 
 @testset "Sum times" begin
@@ -39,4 +43,22 @@ end
     test_sum = JuliaMoney.add(five_dollar, five_dollar)
     test_dollar = JuliaMoney.reduce(aBank, JuliaMoney.times(test_sum, 2), "USD")
     @test test_dollar == JuliaMoney.dollar((5 + 5) * 2)
+end
+
+@testset "minus" begin
+    aBank = JuliaMoney.Bank()
+    one_dollar = JuliaMoney.dollar(1)
+    two_dollar = JuliaMoney.dollar(2)
+    test_dollar = JuliaMoney.reduce(aBank, JuliaMoney.minus(two_dollar, one_dollar), "USD")
+    @test test_dollar == JuliaMoney.dollar(2 - 1)
+
+    rate = 2.0
+    JuliaMoney.add_rate!(aBank, "USD", "CHF", rate)
+    two_franc = JuliaMoney.franc(2)
+    test_dollar = JuliaMoney.reduce(aBank, JuliaMoney.minus(two_franc, two_dollar), "USD")
+    @test test_dollar == JuliaMoney.dollar(2 / rate - 2)
+
+    two_franc_plus_one_dollar = JuliaMoney.add(two_franc, one_dollar)
+    test_dollar = JuliaMoney.reduce(aBank, JuliaMoney.minus(two_dollar, two_franc_plus_one_dollar), "USD")
+    @test test_dollar == JuliaMoney.dollar(2 - (2 / rate + 1))
 end
